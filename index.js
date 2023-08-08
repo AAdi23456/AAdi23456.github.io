964c32ac13cb0b06fefedddb99c472cf55fb580a
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-08-09 00:00:11 +0530

    done

--- index.js
+++ index.js
@@ -73,12 +73,12 @@ var typed = new Typed(".typing", {
 
 document.querySelector('#resume-link-1').addEventListener("click", () => {
     // console.log("OPENinig.....")
-    window.location.assign("https://drive.google.com/file/d/1bIGacWFyWLmHhRK3uYJ-MQTR_Hs1O096/view?usp=sharing", "_blank");
+    window.location.assign("https://drive.google.com/file/d/1FxTxigvjKUtAQUwxUO4ljouRc8kxYqez/view?usp=sharing", "_blank");
 })
 
 document.querySelector('#resume-link-2').addEventListener("click", () => {
     // console.log("OPENinig.....")
-    window.location.assign("https://drive.google.com/file/d/1bIGacWFyWLmHhRK3uYJ-MQTR_Hs1O096/view?usp=sharing", "_blank");
+    window.location.assign("https://drive.google.com/file/d/1FxTxigvjKUtAQUwxUO4ljouRc8kxYqez/view?usp=sharing", "_blank");
 })
 
 
